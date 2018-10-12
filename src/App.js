acab96038f25420fb01fa1977468cc33236ba19a
Belinda Galiano <belindagalianomolina@gmail.com>
2018-10-12 14:38:19 -0600

    First commit. Refactoring Portfolio to React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,234 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header>
+          <div className="top-bar mdc-elevation--z2">
+            <h1 className="mdc-typography--headline3 title-style">Belinda Galiano Projects</h1>
+            <nav className="nav-content">
+              <a href="index.html" className="mdc-typography--headline6 a-web">
+                Projects
+        </a>
+              <a href="about.html" className=" mdc-typography--headline6 a-web">
+                About Me
+        </a>
+              <a href="contact.html" className=" mdc-typography--headline6 a-web">
+                Contact
+        </a>
+            </nav>
+          </div>
         </header>
-      </div>
+
+        <main>
+          <div className="content-center-row">
+            <h2 className="mdc-typography--headline4 mdc-theme--primary">PROJECTS</h2>
+          </div>
+
+          <div className="content-center-row">
+            <div className="mdc-card marging-top">
+              <div className="my-card__media mdc-card__media mdc-card__media--16-9"></div>
+              <div>
+                <div className="mdc-typography--headline6">LookingOut</div>
+                <div className="mdc-typography--body2">React/Reach Router/JS/HTML/CSS - This allows users to look for images of planets and stars from the NASA Library</div>
+              </div>
+              <div className="mdc-card__actions">
+                <div className="mdc-card__action-buttons">
+                  <a href="https://lookingout-df5c3.firebaseapp.com/" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+                </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="mdc-card marging-top">
+              <div className="my-card__media mdc-card__media mdc-card__media--16-9"></div>
+              <div>
+                <div classNameName="mdc-typography--headline6"> Burrito Guy Order Form</div>
+                <div classNameName="mdc-typography--body2">HTML/CSS/JS - This allows a user to order a burrito online </div>
+              </div>
+              <div classNameName="mdc-card__actions">
+                <div classNameName="mdc-card__action-buttons">
+                  <a href="api-drills/drills/version-2/index.html" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+            </a>
+                  <a href="https://github.com/belinda-galiano/projects/tree/master/api-drills/version-2" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Github
+            </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="mdc-card marging-top">
+              <div clas></div>
+              <div >
+                <div className="mdc-typography--headline6">Stranger Things Telephone</div>
+                <div className="mdc-typography--body2">HTML/CSS/JS - This allows the user to send a message</div>
+              </div>
+              <div className="mdc-card__actions">
+                <div className="mdc-card__action-buttons">
+                  <a href="api-drills/drills/version-1/index.html" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+            </a>
+                  <a href="https://github.com/belinda-galiano/projects/tree/master/api-drills/version-1" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Github
+            </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="mdc-card marging-top">
+              <div className="my-card__media mdc-card__media mdc-card__media--16-9"></div>
+              <div>
+                <div className="mdc-typography--headline6">Pixar Art Maker</div>
+                <div className="mdc-typography--body2">HTML/CSS/JS - This allows a user to choose colors from a palette and then paint
+            pixel art</div>
+              </div>
+              <div className="mdc-card__actions">
+                <div className="mdc-card__action-buttons">
+                  <a href="dom-drills/drills/version-3/index.html" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+            </a>
+                  <a href="https://github.com/belinda-galiano/projects/tree/master/dom-drills/version-3" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Github
+            </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="mdc-card marging-top">
+              <div className="my-card__media mdc-card__media mdc-card__media--16-9"></div>
+              <div>
+                <div className="mdc-typography--headline6">Basic Calculator</div>
+                <div className="mdc-typography--body2">HTML/JS</div>
+              </div>
+              <div className="mdc-card__actions">
+                <div className="mdc-card__action-buttons">
+                  <a href="dom-drills/drills/version-2/index.html" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+            </a>
+                  <a href="https://github.com/belinda-galiano/projects/tree/master/dom-drills/version-2" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Github
+            </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="mdc-card marging-top">
+              <div className="my-card__media mdc-card__media mdc-card__media--16-9"></div>
+              <div>
+                <div className="mdc-typography--headline6">Tic Tac Toe</div>
+                <div className="mdc-typography--body2">HTML/JS - Game</div>
+              </div>
+              <div className="mdc-card__actions">
+                <div className="mdc-card__action-buttons">
+                  <a href="dom-drills/drills/version-1/index.html" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+            </a>
+                  <a href="https://github.com/belinda-galiano/projects/tree/master/dom-drills/version-1" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Github
+            </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="mdc-card marging-top">
+              <div className="my-card__media mdc-card__media mdc-card__media--16-9"></div>
+              <div>
+                <div className="mdc-typography--headline6">A Journey to become a Front End Web Developer</div>
+                <div className="mdc-typography--body2">HTML/CSS - Project Bucket 1</div>
+              </div>
+              <div className="mdc-card__actions">
+                <div className="mdc-card__action-buttons">
+                  <a href="html-css-drills/galvanize-learnr/index.html" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+            </a>
+                  <a href="https://github.com/belinda-galiano/projects/tree/master/html-css-drills/project-bucket-1" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Github
+            </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="mdc-card marging-top">
+              <div className="my-card__media mdc-card__media mdc-card__media--16-9"></div>
+              <div>
+                <div className="mdc-typography--headline6">Café Versa</div>
+                <div className="mdc-typography--body2">HTML/CSS - Multi-page Website for a Restaurant</div>
+              </div>
+              <div className="mdc-card__actions">
+                <div className="mdc-card__action-buttons">
+                  <a href="html-css-drills/build-basic-websites/drills/version-3/index.html" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+            </a>
+                  <a href="https://github.com/belinda-galiano/projects/tree/master/html-css-drills/build-basic-websites/version-3" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Github
+            </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="mdc-card marging-top">
+              <div className="my-card__media mdc-card__media mdc-card__media--16-9"></div>
+              <div>
+                <div className="mdc-typography--headline6">Dulcini's Blog - A way to enjoy life</div>
+                <div className="mdc-typography--body2">HTML/CSS - Landing page for a blog</div>
+              </div>
+              <div className="mdc-card__actions">
+                <div className="mdc-card__action-buttons">
+                  <a href="html-css-drills/build-basic-websites/drills/version-2/index.html" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+            </a>
+                  <a href="https://github.com/belinda-galiano/projects/tree/master/html-css-drills/build-basic-websites/version-2" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Github
+            </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="mdc-card marging-top">
+              <div className="my-card__media mdc-card__media mdc-card__media--16-9"></div>
+              <div>
+                <div className="mdc-typography--headline6">EJet - when the world is your home</div>
+                <div className="mdc-typography--body2">HTML/CSS - Landing page for a company or business</div>
+              </div>
+              <div className="mdc-card__actions">
+                <div className="mdc-card__action-buttons">
+                  <a href="html-css-drills/build-basic-websites/drills/version-1/index.html" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Website
+            </a>
+                  <a href="https://github.com/belinda-galiano/projects/tree/master/html-css-drills/build-basic-websites/version-1" className="mdc-button mdc-card__action mdc-card__action--button beli-button"
+                    target="_blank" rel="noopener">
+                    Github
+            </a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </main>
+      </div >
     );
   }
 }
